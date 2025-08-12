555ae666722fdfc262b78edfe88a61907e915979
Valerie Valentin <valerie.valentine93@gmail.com>
2025-08-12 02:49:11 -0700

    Created my guess & board classes

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    public int lives;
+    private String[] answer;
+    public ArrayList<Guess> guessHistory;
+    public String gameStatus;
+
+
+
 }
--- src/model/Guess.java
+++ src/model/Guess.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Guess {
+    private String[] guess;
+    private int correctNumber;
+    private int correctLocation;
+
+    public Guess(String[] guess){
+        this.guess = guess;
+    }
+
+    public void setCorrectLocation(int correctLocation) {
+        this.correctLocation = correctLocation;
+    }
+
+    public void setCorrectNumber(int correctNumber) {
+        this.correctNumber = correctNumber;
+    }
+
+    public String[] getGuess() {
+        return guess;
+    }
+
+    public int getCorrectLocation() {
+        return correctLocation;
+    }
+
+    public int getCorrectNumber(){
+        return correctNumber;
+    }
 }
