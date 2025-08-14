a0c8578b4fb648ff8901f636aa71d872d612fa37
Valerie Valentin <valerie.valentine93@gmail.com>
2025-08-13 18:09:23 -0700

    refactored timer into a runnable interface so it can run on a independent thread

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import controller.Game;
 import model.Timer;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 //        Game game = new Game();
 //        game.startGame();
-        Timer.clock(1);
+        Timer.run(1);
     }
 }
--- src/model/Timer.java
+++ src/model/Timer.java
@@ -1,18 +1,31 @@
 package model;
 
-public class Timer {
+public class Timer implements Runnable {
+    private int minutes;
 
-    public static void clock (int minutes) throws InterruptedException {
+    public Timer(int minutes) {
+        this.minutes = minutes;
+    }
+
+    @Override
+    public void run() {
         int seconds = minutes * 60;
 
         while (seconds > 0) {
             int displayMinutes = seconds / 60;
             int displaySeconds = seconds % 60;
             System.out.printf("\rTime left: %02d:%02d", displayMinutes, displaySeconds);
-            Thread.sleep(1000);
-            seconds--;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted!");
             }
-            System.out.println();
-            System.out.println("Time's up!");
+
+            seconds--;
         }
+
+        System.out.println();
+        System.out.println("Time's up!");
     }
+}
